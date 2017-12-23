8bfa1b4cf528782ab8ce8eadaafffc90e96f3e98
Marcus Ackland <mackland@kth.se>
2017-12-22 18:19:50 -0800

    remove controls, put in seprate controller class

--- view/Display.java
+++ view/Display.java
@@ -1,22 +1,12 @@
 package view;
 
-import java.io.*;
-
 public class Display{
-    public char getDirections() {
-        char inChar = 's';
-
-        System.out.println("Where do you want to go?...");
-        System.out.println("North (n)");
-        System.out.println("South (s)");
-        System.out.println("West (w)");
-        System.out.println("East (e)");
-        try{
-            inChar = (char) System.in.read();
-        } catch(IOException ioe) {
-            System.out.println("Invalid input!");
-        }
-        return inChar;
+    public void getDirections() {
+        System.out.println("--- Where do you want to go? ---");
+        System.out.println("1. North.....(n)");
+        System.out.println("2. South.....(s)");
+        System.out.println("3. West......(w)");
+        System.out.println("4. East......(e)");
     }
 
     public void enemyAppeared(){
