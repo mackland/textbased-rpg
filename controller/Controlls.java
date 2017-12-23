2b2875ae36d20e692e5e80f4ba09ddb49ad5bfea
Marcus Ackland <mackland@kth.se>
2017-12-22 18:19:03 -0800

    try to separate user input in separate class

---
+++ controller/Controlls.java
@@ -0,0 +1,47 @@
+package controller;
+
+import model.*;
+import view.*;
+
+import java.io.*;
+
+public class Controlls{
+    public void startGame(){
+        World game = new World();
+        Display display = new Display();
+        Player player1 = new Player(20, 20, 4, 0, "Marcus");
+        Creature creature = new Creature(20, 20, 4, 0, "Skeleton");
+        Battle battle;
+        
+        game.setUp();
+        game.printBoard();
+        display.getDirections();
+        char input = getInput();
+        game.updatePosition(input);
+        
+        //model update position
+        //when enter new position on board
+        //random chance to fight enemy
+
+        display.enemyAppeared();
+        //game.startBattle();
+        //battle = new Battle(player1, creature, display);
+        //battle.start();
+    }
+
+    public char getInput(){
+        char inChar = 'x';
+        try{
+            inChar = (char) System.in.read();
+        } catch(IOException ioe) {
+        
+        }
+        return inChar;
+    }
+
+    public static void main(String[] args){
+        Controlls c = new Controlls();
+        c.startGame();
+    }
+
+}
