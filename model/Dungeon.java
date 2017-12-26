84b74a4bb03ca0083f08bfe6a3b3e77671cd6071
Marcus Ackland <mackland@kth.se>
2017-12-26 15:40:59 -0800

    Added classes for dungeons and rooms

---
+++ model/Dungeon.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Dungeon{
+    private static boolean northDirection = false;
+    private static boolean southDirection = false;
+    private static boolean westDirection = false;
+    private static boolean eastDirection = false;
+
+    private static boolean cleared = false;
+
+    public static Room[][] newDungeon(Player player){
+        Room[][] dungeon = new Room[10][10];
+        Room[4][4] = Room.newRoom();
+    
+    }
+
+}
---
+++ model/Room.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Room{
+    private static boolean exit = false;
+    private static int numOfMonsters = 0;
+}
